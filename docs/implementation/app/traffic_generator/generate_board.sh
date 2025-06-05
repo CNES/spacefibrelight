cf040befd9e9ef130d5a2de3f8ab53f397cdf98c
Florent Manni (Albertine) <florent.manni@cnes.fr>
2025-06-05 15:36:29 +0200

    change html_baseurl in sphinx to https://cnes.github.io/spacefibrelight/

--- docs/implementation/app/traffic_generator/generate_board.sh
+++
@@ -1,4 +0,0 @@
-#use you own environnement variable SPWFV_ROOTPATHto set the root path of the spacefibrelight project
-# this env variable is used in the xilinx tcl script create_project.tcl 
-
-SPWF_ROOTPATH=../../../ /opt/Xilinx/Vivado/2024.1/bin/vivado -nolog -nojournal -mode batch -source create_project.tcl 
